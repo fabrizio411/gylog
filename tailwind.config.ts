4df49e1c48c6cfc0dd73a3afa152773d28a3aec8
fabrizio411 <fabriziomourglia@gmail.com>
2023-10-23 23:14:23 -0300

    Initial Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,22 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'dark-1': '#101010',
+        'dark-2': '#181818',
+        'dark-darker': '#0f0f0f',
+        'dark-hover': '#252525',
+        'dark-border': '#4b4b4b',
+        'dark-border-2': '#1e1e1e',
+        'light-1': '#FFFFFF',
+        'light-2': '#9e9e9e',
+        'light-3': '#6e6e6e',
+        'blue-1': '#6e6e6e',
+        'blue-2': '#6e6e6e',
+        'blue-3': '#6e6e6e',
+        'blue-hover': '#3b94ec',
+        'red-1': '#ba1a1a'
+      }
     },
   },
   plugins: [],
